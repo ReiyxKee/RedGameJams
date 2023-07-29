2d8f3b394e6068d3abc7a262e3eb138ba61dad7e
Strachan <strachan.chan@gmail.com>
2023-07-29 12:09:40 +0800

    Updated Drag by reiyx

---
+++ GameJam/Assets/Script/GridButton.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GridButton : MonoBehaviour
+{
+    [SerializeField] private Button button;
+    [SerializeField] private bool isToggled = false;
+    [SerializeField] private GameObject sprite;
+
+    private void Start()
+    {
+        // Assign the onClick event to the ToggleButton function
+        button.onClick.AddListener(ToggleButton);
+    }
+
+    private void ToggleButton()
+    {
+        // Toggle 
+        isToggled = !isToggled;
+
+        sprite.SetActive(isToggled);
+        
+    }
+}
